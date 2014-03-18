b9e818294d7973510588d52853bea270127598e4
Gareth Townsend <gareth.townsend@me.com>
2014-03-18 19:46:48 +1100

    Generate new rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FitService::Application.config.secret_key_base = '89d04283497f7a5b8f21831cb0a8b375121fe06e93932cff201fab179fb90f5d10289dad32e4568b7065e954b5d4860752fb1187ed0fc86e1083e909e8e0cde2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FitService::Application.config.session_store :cookie_store, key: '_fit-service_session'
